dbc642ad6210638e6bddee642ba14059235b4e65
Nicholas Pease <me@nicholaspease.com>
2025-07-06 22:01:01 -0400

    feat: re-add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,12 @@ export default defineConfig({
     quasar({
       sassVariables: fileURLToPath(new URL('./src/styles/quasar.variables.scss', import.meta.url)),
     }),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+          enabled: true
+        } 
+    })
   ],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
@@ -22,5 +29,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: "AnNER/"
+  base: '/AnNER/'
 })
